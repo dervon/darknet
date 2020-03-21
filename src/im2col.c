6d673bbb7e5ffe5c89f3a58fdb9069451b4b6980
Dervon <dervon.he@outlook.com>
2020-03-21 15:53:05 +0800

    im2col方法注解

--- src/im2col.c
+++ src/im2col.c
@@ -13,9 +13,15 @@ float im2col_get_pixel(float *im, int height, int width, int channels,
 
 //From Berkeley Vision's Caffe!
 //https://github.com/BVLC/caffe/blob/master/LICENSE
-void im2col_cpu(float* data_im,
+/*
+将channels*height*width的输入图像与channels*ksize*ksize的卷基核进行步长为stride，零填充为pad的卷基运算转换成矩阵运算，可以调用gemm
+(height + 2*pad - ksize) / stride + 1）* （(width + 2*pad - ksize) / stride + 1）* （channels * ksize * ksize）
+|-------------输出图像的高-------------| * |-------------输出图像的宽--------------|
+|------------------------------------矩阵的列-----------------------------------| * |--------矩阵的行----------|
+*/
+void im2col_cpu(float* data_im/*输入图像*/,
      int channels,  int height,  int width,
-     int ksize,  int stride, int pad, float* data_col) 
+     int ksize/*滤波器尺寸*/,  int stride/*步长*/, int pad/*零填充*/, float* data_col/*输出矩阵*/) 
 {
     int c,h,w;
     int height_col = (height + 2*pad - ksize) / stride + 1;
@@ -31,6 +37,7 @@ void im2col_cpu(float* data_im,
                 int im_row = h_offset + h * stride;
                 int im_col = w_offset + w * stride;
                 int col_index = (c * height_col + h) * width_col + w;
+                /*根据计算出来的坐标去原图data_im取像素值，注意此时的坐标是加上了零填充pad后的坐标*/
                 data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
                         im_row, im_col, c_im, pad);
             }
